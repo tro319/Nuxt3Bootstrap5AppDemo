eb345c3413d8e5c1a085cbe00d01c21963868c9d
tro319 <149455937+tro319@users.noreply.github.com>
2023-11-14 05:58:05 +0000

    animatecss追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="main">
         <h2>最近の観戦記録↓</h2>
-            <div class="img">
+            <div ref="target" class="img animate__animated animate__zoomInLeft">
                 <img src="../assets/images/1.jpg">
             </div>
-            <div class="img">
+            <div ref="target" class="img">
                 <img src="../assets/images/2.jpg">
             </div>
-            <div class="img">
+            <div ref="target" class="img">
                 <img src="../assets/images/3.jpg">
             </div>
         </div>
