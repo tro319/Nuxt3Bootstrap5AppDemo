d9b43c2b0bc9e845439160dd7706a641c58d698a
tro319 <149455937+tro319@users.noreply.github.com>
2023-11-21 05:17:20 +0000

    chanderのサイト追加

--- pages/pagesin/chander.vue
+++ pages/pagesin/chander.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container">
-    <h1>chander</h1>
+  <div class="contents">
+  <div class="contents-title">
+    <h2>Chander</h2>
+  </div>
+  <div class="img">
+    <img src="../../public/1.jpg">
+  </div>
   </div>
 </template>
