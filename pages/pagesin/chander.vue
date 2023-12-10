bd12c90e30ed84316b8fc0ba3919750a53e984c8
tro319 <149455937+tro319@users.noreply.github.com>
2023-12-10 09:43:38 +0000

    ページトップのお店のリストに、グラデーションかける

--- pages/pagesin/chander.vue
+++ pages/pagesin/chander.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="contents animate__animated animate__fadeIn">
-    <div class="link-top">
   <div class="contents-title">
     <h2>Chander</h2>
   </div>
@@ -11,13 +10,13 @@
     <li><NuxtLink to="#imformation">店舗情報</NuxtLink></li>
     <li><NuxtLink to="#menu">メニュー</NuxtLink></li>
   </ul>
-</div>
   <div class="img">
     <img src="../../public/1.jpg">
   </div>
-  <div>
+  <div id="imformation">
     chander京都北山店<br>
     ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
+    <br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
   </div>
   </div>
 </template>
