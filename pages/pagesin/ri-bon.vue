e34301dba9a5aa487bcb094265c188810fead996
tro319 <149455937+tro319@users.noreply.github.com>
2023-12-12 04:50:14 +0000

    リンクページ追加2

---
+++ pages/pagesin/ri-bon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="contents animate__animated animate__fadeIn">
+  <div class="contents-title">
+    <h2>一膳飯屋 りぃぼん</h2>
+  </div>
+  <div class="sub-title">
+    <h3>上賀茂神社近くの、日本式洋食レストラン!</h3>
+  </div>
+  <ul class="list">
+    <li><NuxtLink to="#imformation">店舗情報</NuxtLink></li>
+    <li><NuxtLink to="#menu">メニュー</NuxtLink></li>
+  </ul>
+  <div class="img">
+  </div>
+  <div id="imformation">
+    一膳飯屋 りぃぼん<br>
+    ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
+    <br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
+  </div>
+  </div>
+</template>
