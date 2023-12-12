0aee996ad412dc38ca33db7c520deb9dfc6c042b
tro319 <149455937+tro319@users.noreply.github.com>
2023-12-12 04:24:40 +0000

    トップページリンクトランジション追加

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,9 +3,9 @@
     <h1><NuxtLink class="home-link" to="/">Shotaro</NuxtLink></h1>
     <div id="nav">
 <ul class="shop-list">
-<li><NuxtLink to="pagesin/ri-bon">リーボン</NuxtLink></li>
-<li><NuxtLink to="pagesin/mokkosu">肥後もっこす</NuxtLink></li>
-<li><NuxtLink to="pagesin/chander">chander</NuxtLink></li>
+<li><NuxtLink to="/pagesin/ri-bon">リーボン</NuxtLink></li>
+<li><NuxtLink to="/pagesin/mokkosu">肥後もっこす</NuxtLink></li>
+<li><NuxtLink to="/pagesin/chander">chander</NuxtLink></li>
 </ul>
 </div>
   </div>
--- pages/pagesin/mokkosu.vue
+++ pages/pagesin/mokkosu.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="contents">
-    <div class="contents-title">
-        <h2>肥後もっこす</h2>
-    </div>
-    </div>
+  <div class="contents animate__animated animate__fadeIn">
+  <div class="contents-title">
+    <h2>肥後もっこす</h2>
+  </div>
+  <div class="sub-title">
+    <h3>北山駅でてすぐのラーメン屋!</h3>
+  </div>
+  <ul class="list">
+    <li><NuxtLink to="#imformation">店舗情報</NuxtLink></li>
+    <li><NuxtLink to="#menu">メニュー</NuxtLink></li>
+  </ul>
+  <div class="img">
+  </div>
+  <div id="imformation">
+    肥後もっこす<br>
+    ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
+    <br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>ああ<br>あ
+  </div>
+  </div>
 </template>
