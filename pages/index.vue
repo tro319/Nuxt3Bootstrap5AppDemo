949e891ecc7bf8aaa7585b94fcbcb6ae2956a5a8
tro319 <149455937+tro319@users.noreply.github.com>
2024-01-14 11:26:31 +0000

    店舗ページレイアウト調整

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,5 @@
 <div>
 </div>
 </template>
-<script setup lang="ts">
-const targetAbout = ref<HTMLElement | null>(null)
-const targetAboutListTitle = ref<HTMLElement | null>(null)
 
-const elements = [
-    targetAbout,
-    targetAboutListTitle,
-]
-
-onMounted(() => {
-    useIntersectionObserver().doObserve(elements)
-})
-</script>
 
