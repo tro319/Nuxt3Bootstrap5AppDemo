f21db26119fc2908e0af4fb74ac3bcef3a8e3d3f
tro319 <149455937+tro319@users.noreply.github.com>
2024-02-01 09:47:34 +0000

    bootstrap 修正完成

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
     <img class="mw-100" src="../public/main-top.jpg" alt="上賀茂神社">
 </div>
 <ul class="img-list list-unstyled text-center">
-    <li class="mw-100 mb-5"><img src="../public/main-list1.jpg" alt="画像1"></li>
-    <li class="mw-100 mb-5"><img src="../public/main-list2.jpg" alt="画像2"></li>
-    <li class="mw-100 mb-5"><img src="../public/main-list3.jpg" alt="画像3"></li>
+    <li class="mb-5"><img class="mw-100" src="../public/main-list1.jpg" alt="画像1"></li>
+    <li class="mb-5"><img class="mw-100" src="../public/main-list2.jpg" alt="画像2"></li>
+    <li class="mb-5"><img class="mw-100" src="../public/main-list3.jpg" alt="画像3"></li>
 </ul>
 </div>
 </template>
